713febbaa664b57d19edcd161ba5d5093ca235a3
Marco Pivetta <ocramius@gmail.com>
2022-09-01 10:58:27 +0200

    Exercise 2: track last temperatures in a file under `data/`

--- exercises/exercise-02/project-last-temperature.php
+++ exercises/exercise-02/project-last-temperature.php
@@ -5,8 +5,17 @@ declare(strict_types=1);
 
 namespace EventSourcingWorkshop\Exercise\TemperatureTracking;
 
-use BadMethodCallException;
 use EventSourcingWorkshop\Glue\Application\Kernel;
+use EventSourcingWorkshop\TemperatureTracking\Domain\TemperatureRecorded;
+
+use function file_exists;
+use function file_put_contents;
+use function json_encode;
+use function Psl\File\read;
+use function Psl\Json\typed;
+use function Psl\Type\dict;
+use function Psl\Type\float;
+use function Psl\Type\non_empty_string;
 
 (static function (): void {
     require_once __DIR__ . '/../../vendor/autoload.php';
@@ -18,12 +27,34 @@ use EventSourcingWorkshop\Glue\Application\Kernel;
     /**
      * Here we want to:
      *
-     * @TODO 1. iterate over recorded temperatures (tip: check the `$kernel`'s {@see Kernel::$traverseEventStream})
-     * @TODO 2. generate an `array<string, float>` containing the last known temperature at each location
-     * @TODO 3. save all accumulated temperatures to a file
+     * 1. iterate over recorded temperatures (tip: check the `$kernel`'s {@see Kernel::$traverseEventStream})
+     * 2. generate an `array<string, float>` containing the last known temperature at each location
+     * 3. save all accumulated temperatures to a file
      *
      * Question: what happens when you run this script multiple times?
      * Question: can you record new temperatures, and track them?
      */
-    throw new BadMethodCallException('Incomplete: remove me once finished with the exercise!');
+
+    /** @var array<non-empty-string, float> $temperatures */
+    $temperatures = [];
+
+    if (file_exists(__DIR__ . '/../../data/last-temperatures.json')) {
+        $temperatures = typed(
+            read(__DIR__ . '/../../data/last-temperatures.json'),
+            dict(
+                non_empty_string(),
+                float(),
+            ),
+        );
+    }
+
+    foreach (($kernel->traverseEventStream)('last-recorded-temperatures') as $event) {
+        if (! $event instanceof TemperatureRecorded) {
+            continue;
+        }
+
+        $temperatures[$event->location] = $event->celsius;
+    }
+
+    file_put_contents(__DIR__ . '/../../data/last-temperatures.json', json_encode($temperatures));
 })();

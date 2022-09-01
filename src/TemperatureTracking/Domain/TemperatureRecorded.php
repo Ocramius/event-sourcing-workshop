011503d717476b94afbdfef7c2a365fd4cd962e2
Marco Pivetta <ocramius@gmail.com>
2022-09-01 10:23:19 +0200

    Exercise 1: recording a temperature

---
+++ src/TemperatureTracking/Domain/TemperatureRecorded.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSourcingWorkshop\TemperatureTracking\Domain;
+
+use DateTimeImmutable;
+use EventSourcingWorkshop\EventSourcing\Domain\DomainEvent;
+
+/** @psalm-immutable */
+final class TemperatureRecorded implements DomainEvent
+{
+    /** @param non-empty-string $location */
+    public function __construct(
+        public readonly string            $location,
+        public readonly float             $celsius,
+        public readonly DateTimeImmutable $raisedAt,
+    ) {
+    }
+
+    public function raisedAt(): DateTimeImmutable
+    {
+        return $this->raisedAt;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'location' => $this->location,
+            'celsius'  => $this->celsius,
+            'raisedAt' => $this->raisedAt->format(DateTimeImmutable::RFC3339_EXTENDED),
+        ];
+    }
+}
